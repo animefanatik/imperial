eb3f06df90f08bbf5062a8886c6fcbfda504e7fb
jafet baltodano <animefanatik@gmail.com>
2020-04-07 08:37:35 -0600

    grunt config

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		/**
+		 * Sass
+		 */
+		sass: {
+		  dev: {
+		    options: {
+              style: 'compressed',
+              sourcemap: false
+		    },
+		    files: {
+				'../css/style.css': 'scss/style.scss',
+		    }
+		  }
+		},
+
+	  	/**
+	  	 * Watch
+	  	 */
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
+		},
+		
+		imagemin: {
+			dynamic: {
+				options: {
+					optimizationLevel: 6, 
+					progressive: true
+				},
+				files: [{
+					expand: true,
+					cwd: 'img/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: '../img/'
+				}]
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.registerTask('default',['watch', 'imagemin']);
+}
